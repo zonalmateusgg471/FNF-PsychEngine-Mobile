c89518e33e944c9e29a34bfca6b790e8e972ea59
Lily <egzozu.be.bas@gmail.com>
2024-09-29 12:06:59 +0300

    Update CopyState.hx

--- source/mobile/states/CopyState.hx
+++ source/mobile/states/CopyState.hx
@@ -16,7 +16,7 @@ class CopyState extends MusicBeatState
 {
 	private static final textFilesExtensions:Array<String> = ['ini', 'txt', 'xml', 'hxs', 'hx', 'lua', 'json', 'frag', 'vert'];
 	public static final IGNORE_FOLDER_FILE_NAME:String = "ignore.txt";
-	private static var directoriesToIgnore:Array<String> = [];
+	private var directoriesToIgnore:Array<String> = [];
 	public static var locatedFiles:Array<String> = [];
 	public static var maxLoopTimes:Int = 0;
 
